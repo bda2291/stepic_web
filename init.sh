d598751b27c93cda4489c3c799692087ee852f26
bda2291 <bda2291@mail.ru>
2016-03-15 09:42:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn2.conf   /etc/gunicorn.d/test2
 sudo /etc/init.d/gunicorn restart
